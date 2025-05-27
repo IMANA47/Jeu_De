08d5b4618df83280d0fa124b7bd05fbeba9a90c7
François Nsengimana <francknsengimana@gmail.com>
2025-05-27 23:26:38 +0100

    developpement de l'interface graphique et ajout des bouton action

--- Jeu_De.py
+++ Jeu_De.py
@@ -2,6 +2,12 @@ import random
 from tkinter import *
 from PIL import ImageTk, Image
 
+
+#les fonction
+def jouer_de():
+    pass
+
+
 jeu = Tk()
 jeu.title("Jeu De")
 jeu.geometry('400x500+500+100')
@@ -18,15 +24,25 @@ numero_choisi.grid(column=1, row=2, columnspan=2, padx=10, pady=10)
 # liste des image a recuperer
 liste_images = ['images/de1.png', 'images/de2.png', 'images/de3.png', 'images/de4.png','images/de5.png' , 'images/de6.png' ]
 
+# pour afficher les image et recuperer de maniere aleatoire
 image_de = ImageTk.PhotoImage(Image.open(random.choice(liste_images)))
-
 label_image = Label(jeu, image=image_de)
 label_image.image = image_de
 label_image.grid(column=1, row=3, columnspan=2)
 
+# Boutton pour lancer le jeu
+bouton_lancer = Button(jeu, text="Lance De", fg="yellow",bg="black", width=15, height=3,command= jouer_de)
+bouton_lancer.grid(column=1, row=4,padx=10, pady=10)
 
+# Boutton pour quitter le jeu
+bouton_lancer = Button(jeu, text="Quitter", fg="white",bg="red", width=15, height=3,command=jeu.quit)
+bouton_lancer.grid(column=2, row=4,padx=10, pady=10)
 
+#Pour afficher le resultat
 
+label_resultat = Label(jeu, text="allo", fg="black",pady=20, width=15, height=3)
+
+label_resultat.grid(column=1, row=5, columnspan=2)
 
 
 jeu.mainloop()
